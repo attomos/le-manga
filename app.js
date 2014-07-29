93de7d78fa6ac5db77af7d6182a63b35a331bfa6
Nattaphoom Ch <attomos@gmail.com>
2014-07-29 22:58:58 +0700

    In the middle of experiment with koa. :bowtie:

---
+++ app.js
@@ -0,0 +1,19 @@
+var views = require('co-views');
+var route = require('koa-route');
+var koa = require('koa');
+var app = koa();
+
+var render = views(__dirname + '/views', { ext: 'ejs' });
+
+app.use(route.get('/', list));
+
+function *list() {
+  this.body = yield render('index', {});
+}
+
+
+app.use(function *() {
+  this.body = 'Hello World';
+});
+
+app.listen(3000);
